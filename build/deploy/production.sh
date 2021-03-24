af426eeeac7d2576685b55341195db8518f939f9
Fabiano Couto <facodeli@gmail.com>
2021-03-24 17:36:44 -0300

    padrao

---
+++ build/deploy/production.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SSH_USER="$1"
+SSH_PASS="$2"
+GIT_USER="$3"
+GIT_PASS="$4"
+expect -c "
+	set timeout 10
+	spawn ssh 151.106.98.0 -p 65002 -l ${SSH_USER}
+	expect \"*connecting*\"
+	send \"yes\r\"
+	expect \"*?sword:*\"
+	send \"${SSH_PASS}\r\"
+	expect \"*login*\"
+	send \"cd domains/inicial.com.br/apps/incoder\r\"
+	send \"git pull -f --no-edit https://${GIT_USER}:${GIT_PASS}@github.com/inicialsolucoes/incoder.git master\r\"
+	expect \"*master*\"
+	send \"exit\r\"
+	expect \"*closed*\"
+	send \"exit\r\"
+	interact
+"
+exit
